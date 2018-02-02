18fdf545a513509c5ee980db29645934461afca9
obogi <obogi85@gmail.com>
2018-02-02 14:33:48 +0900

    URL연결

--- django/polls/models.py
+++ django/polls/models.py
@@ -1,9 +1,9 @@
-from datetime import timezone, datetime
+import datetime
 
 from django.db import models
+from django.utils import timezone
 
 
-# Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -12,7 +12,11 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        """ 이 question의 게시일자가 현재 시각에서 1일 전보다 이후 """
+        """
+        이 Question의 게시일자가 현재시각에서 1일 전보다 이후인지를 리턴
+            (Question을 게시한지 1일이 지나지 않았는지)
+        :return:
+        """
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
